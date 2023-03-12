ea92b4be6e4c94a70a7d3c5f070a4c953840ae1e
Arno Hilke <arno.hilke@tum.de>
2023-03-12 16:07:20 +0100

    chore: setup test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,12 @@ import type { Config } from "jest";
 
 const config: Config = {
     preset: "ts-jest",
-    testPathIgnorePatterns: ["/dist/"],
     moduleNameMapper: {
         "^(\\.{1,2}/.*)\\.js$": "$1",
     },
+    testPathIgnorePatterns: ["dist/"],
+    coverageDirectory: "./test/coverage",
+    coverageReporters: ["json", "html"],
 };
 
 export default config;
